744c97091b962a75eb8cce4811eadd706357190d
zurielprogramador <argenis.canton08@gmail.com>
2023-11-08 22:50:19 -0500

    videos-10y11

--- 09-heroesApp/src/app/pages/heroe/heroe.component.ts
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -2,38 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from 'src/app/models/heroe.model';
 import { NgForm } from '@angular/forms';
 import { HeroesService } from 'src/app/services/heroes.service';
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs'
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent implements OnInit{
+export class HeroeComponent implements OnInit {
   heroe: HeroeModel = new HeroeModel();
 
-  constructor( private heroesService: HeroesService){}
+  constructor(private heroesService: HeroesService) {}
 
-  ngOnInit(){
-  }
+  ngOnInit() {}
 
-  guardar(form: NgForm){
-    if(form.invalid) {
+  guardar(form: NgForm) {
+    if (form.invalid) {
       console.log('Formulario no valido');
       return;
     }
 
-  if (this.heroe.id){
-    this.heroesService.actualizarHeroe( this.heroe)
-    .subscribe(resp => {
-      console.log(resp);
-    })
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando información',
+      icon: 'info',
+      allowOutsideClick: false,
+      showConfirmButton: false
+    });
 
-  }else{
-    this.heroesService.crearHeroe( this.heroe)
-    .subscribe(resp => {
-      console.log(resp);
-      this.heroe = resp;
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
+    if (this.heroe.id) {
+      peticion = this.heroesService.actualizarHeroe(this.heroe)
+
+    } else {
+      peticion = this.heroesService.crearHeroe(this.heroe);
+
+    }
+
+    peticion.subscribe(resp =>{
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actualizo correctamente',
+        icon: 'success'
+      })
     })
-  }
 
   }
 }
