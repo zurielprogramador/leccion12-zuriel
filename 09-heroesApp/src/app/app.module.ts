de767905c6cd15e292997775487407932a00803f
zurielprogramador <argenis.canton08@gmail.com>
2023-11-08 21:42:37 -0500

    video6

--- 09-heroesApp/src/app/app.module.ts
+++ 09-heroesApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
@@ -14,7 +14,8 @@ import { HeroesComponent } from './pages/heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 09-heroesApp/src/app/pages/heroe/heroe.component.ts
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent {
+export class HeroeComponent implements OnInit{
+  heroe: HeroeModel = new HeroeModel();
 
+  constructor(){}
+
+  ngOnInit(){
+  }
+
+  guardar(form: NgForm){
+    if(form.invalid) {
+      console.log('Formulario no valido');
+      return;
+    }
+    console.log(form);
+    console.log(this.heroe);
+  }
 }
